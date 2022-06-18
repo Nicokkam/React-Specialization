5ec6f66d5e728cebc967e3e4f3dab290c247a91d
Nicolas Kamien <nicolas@kamien.com.br>
2022-06-18 11:58:17 -0300

    Redux Thunk

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -129,30 +130,53 @@ function RenderComments({ comments, addComment, dishId }) {
 }
 
 const DishDetail = (props) => {
-  return (
-    <div className='container'>
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-          <BreadcrumbItem active>{props.dish.author}</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>{props.dish.author}</h3>
-          <hr />
+  if (props.isLoading) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <Loading />
         </div>
       </div>
-      <div className='row'>
-        <div className='col-12 col-md-5 m-1'>
-          <RenderDish dish={props.dish} />
-        </div>
-        <div className='col-12 col-md-5 m-1'>
-          <RenderComments comments={props.dish != null ? props.comments : []}
-            addComment={props.addComment}
-            dishId={props.dish.id} />
+    );
+  }
+  else if (props.errMess) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <h4>{props.errMess}</h4>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  else if (props.dish != null)
+    return (
+      <div className='container'>
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.author}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.author}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-12 col-md-5 m-1'>
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className='col-12 col-md-5 m-1'>
+            <RenderComments comments={props.comments}
+              addComment={props.addComment}
+              dishId={props.dish.id} />
+          </div>
+        </div>
+      </div>
+    );
+  else
+    return (
+      <div></div>
+    );
 }
 
 export default DishDetail;
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,7 +1,20 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionTypes from './ActionTypes';
 
-export const Dishes = (state = DISHES, action) => { //REDUCER Function
+export const Dishes = (state = {
+  isLoading: true,
+  errMess: null,
+  dishes: []
+}, action) => { //REDUCER Function
   switch (action.type) {
+    case ActionTypes.ADD_DISHES:
+      return { ...state, isLoading: false, errMess: null, dishes: action.payload }; //... is called sprint operator. Takes the state and duplicates it to return a new object
+
+    case ActionTypes.DISHES_LOADING:
+      return { ...state, isLoading: true, errMess: null, dishes: [] };
+
+    case ActionTypes.DISHES_FAILED:
+      return { ...state, isLoading: false, errMess: action.payload, dishes: [] };
+
     default:
       return state;
   }
